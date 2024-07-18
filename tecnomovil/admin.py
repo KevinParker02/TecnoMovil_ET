314570c25630f001f76555f45c9f03594f008b9c
KevinVivanco_02 <131885520+KevinParker02@users.noreply.github.com>
2024-07-18 02:39:20 -0400

    Delete tecnomovil directory

--- tecnomovil/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Celular, Carrito, Categoria
-
-class CelularAdmin(admin.ModelAdmin):
-    exclude = ('stock',)
-
-admin.site.register(Celular, CelularAdmin)
-admin.site.register(Carrito)
-admin.site.register(Categoria)
--- tecnomovil/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Carrito
-
-class AñadirAlCarritoForm(forms.ModelForm):
-    class Meta:
-        model = Carrito
-        fields = ['celular', 'cantidad']
